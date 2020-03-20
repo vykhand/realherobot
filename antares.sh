fbf17cfdce1ea28c91c7b0584571dad9825384b0
snobu <foo@snobu.org>
2020-03-20 15:31:15 +0200

    Still trying to fix opencensus

--- antares.sh
+++ antares.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-apt update
-apt install build-essential -y
-
 pip install --upgrade pip
+pip install https://files.pythonhosted.org/packages/c6/54/d042e60e3be64dcf1637596ef862b67ffa312dc2173d5cb209d18537ac2b/opencensus_ext_azure-1.0.2-py2.py3-none-any.whl
 pip install -r requirements.txt
 
 python app.py
